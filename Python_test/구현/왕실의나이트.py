c4297176007fe853be10b217d787f9f11c0cbfa1
MyunDev <ygj01069@naver.com>
2020-10-21 16:05:24 +0900

    구현

---
+++ Python_test/구현/왕실의나이트.py
@@ -0,0 +1,17 @@
+knight_position = input()
+row = knight_position[1]
+column = int(ord(knight_position[0])) - int(ord('a')) + 1
+
+steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+
+cnt = 0 
+
+for step in steps:
+    next_row = row + step[0]
+    next_column = column + step[1]
+
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        cnt += 1
+
+
+print(cnt)
