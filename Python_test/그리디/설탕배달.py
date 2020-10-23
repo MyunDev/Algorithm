6638388bb169db5bfd4a5c6fdb0ead093dd1573c
MyunDev <ygj01069@naver.com>
2020-10-23 16:10:46 +0900

    설탕배달.py

---
+++ Python_test/그리디/설탕배달.py
@@ -0,0 +1,19 @@
+#백준 설탕배달
+#https://www.acmicpc.net/problem/2839
+
+n = int(input())
+count = 0
+
+while True:
+    if n % 5 == 0:
+        count += n // 5
+        print(count)
+        break
+    n = n - 3
+    count += 1
+    if n < 0:
+        print('-1')
+        break    
+
+
+
