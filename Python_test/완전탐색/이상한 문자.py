8db922416f2210470f7f798b10b900b37019659c
MyunDev <ygj01069@naver.com>
2020-11-17 11:27:37 +0900

    이상한 문자.py

---
+++ Python_test/완전탐색/이상한 문자.py
@@ -0,0 +1,13 @@
+def solution(s):
+    new_list = s.split()
+    new_word = ""
+    for word in new_list:
+        for i, spell in enumerate(word):
+            if i % 2 == 0:
+                new_word += spell.upper()
+            else:
+                new_word += spell.lower()
+        answer += " "
+    
+    return new_word[:-1]
+    
