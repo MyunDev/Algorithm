848a4ed803f5716f436b7866b0d43016ae5b89cb
MyunDev <ygj01069@naver.com>
2020-12-08 16:23:30 +0900

    숫자의표현.py

---
+++ Python_test/완전탐색/숫자의표현.py
@@ -0,0 +1,18 @@
+#https://programmers.co.kr/learn/courses/30/lessons/12924
+
+def solution(n):
+    answer = 1
+    for i in range(1, n//2 + 1):
+        cnt = 0
+        for j in range(i, n+1): 
+            cnt += j 
+            if cnt == n: 
+                answer += 1
+                break
+            elif cnt > n: 
+                break 
+            else: 
+                continue
+    return answer
+
+    
